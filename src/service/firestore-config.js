a2ac1a034cc520d7b8ea34722507c978b3447824
fxghesa <gcoltonwhite84@gmail.com>
2022-07-13 00:45:48 +0700

    Init firebase & firestore

---
+++ src/service/firestore-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+  
+const firebaseConfig = {
+    apiKey: "AIzaSyBbOZqg19S67nkfzis-7SXGvaQzN_GPGks",
+    authDomain: "apps-2ee38.firebaseapp.com",
+    databaseURL: "https://apps-2ee38.firebaseio.com",
+    projectId: "apps-2ee38",
+    storageBucket: "apps-2ee38.appspot.com",
+    messagingSenderId: "545216665155",
+    appId: "1:545216665155:web:5f5d97112ebe366c4566f6"
+};
+  
+const firebaseApp = initializeApp(firebaseConfig);
+export const db = getFirestore(firebaseApp);
