9ad1b1aa426633595373b813b3074fe1d3529d2a
fxghesa <gcoltonwhite84@gmail.com>
2022-08-06 18:38:33 +0700

    add primereact

--- src/App.js
+++ src/App.js
@@ -2,24 +2,45 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import { getUsers } from "./service/users";
 
+import { Dropdown } from 'primereact/dropdown';
+
 function App() {
-  const [users, setUser] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState('');
 
   useEffect(() => {
     async function fetchFirestore() {
-      setUser(await getUsers())
+      const usersDropdown = (await getUsers()).map(x => ({
+        label: x.Name,
+        value: x.UserName
+      }));
+      setUsers(usersDropdown);
     }
     fetchFirestore()
   });
 
   return (
     <div className="App">
-      <h1>Choose a user</h1>
-      <ul>
-        {
-          users.map(x => (<li key={x.UserName}>{x.Name}</li>))
-        }
-      </ul>
+      <br />
+      <br />
+      <i className="pi pi-cloud" style={{'fontSize': '10em'}}></i>
+      <br />
+      <div className="card">
+        <div className="p-fluid grid">
+          <div className="field col-2"></div>
+          <div className="field col-8">
+            <Dropdown
+              loading
+              loadingIcon="pi pi-spin pi-sun"
+              value={user} 
+              options={users} 
+              onChange={(e) => setUser(e.value)} 
+              placeholder="Select a User">
+            </Dropdown>
+          </div>
+          <div className="field col-2"></div>
+        </div>
+      </div>
     </div>
   );
 }
